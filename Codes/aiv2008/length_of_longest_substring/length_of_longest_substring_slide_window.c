76b0982c7fdfb8efecaedc0be3fc8e24867fc5f7
aiv2008 <aiv2005@sina.com>
2019-11-23 02:22:20 -0500

    the length of longest substring with sliding window

--- Codes/aiv2008/length_of_longest_substring/length_of_longest_substring_slide_window.c
+++ Codes/aiv2008/length_of_longest_substring/length_of_longest_substring_slide_window.c
@@ -30,7 +30,7 @@ int h(char key)
 }
 
 
-void put(HashMap **ppHashMap,  char key, int val)
+void put(HashMap **ppHashMap,  char key,signed int val)
 {
 	if(!ppHashMap)return;
 	if(!*ppHashMap)
@@ -47,9 +47,9 @@ void put(HashMap **ppHashMap,  char key, int val)
 		pKeyIndex = (ListNode*)calloc(1, sizeof(ListNode));
 		pValIndex = (ListNode*)calloc(1, sizeof(ListNode));
 		pKeyIndex->val = (char*)calloc(1, sizeof(char));
-		pValIndex->val = (int*)calloc(1, sizeof(int));
+		pValIndex->val = (signed int*)calloc(1, sizeof(signed int));
 		*(char*)(pKeyIndex->val) = key;
-		*(int*)(pValIndex->val) = val;
+		*(signed int*)(pValIndex->val) = val;
 		*((*ppHashMap)->key + hashCode) = pKeyIndex;
 		*((*ppHashMap)->val + hashCode) = pValIndex;
 	}
@@ -64,7 +64,8 @@ void put(HashMap **ppHashMap,  char key, int val)
 		{
 			if(*(char*)(pKeyMove->val) == key)
 			{
-				pValMove->val = &val;
+				//pValMove->val = &val;
+				*(signed int*)(pValMove->val) = val;
 				count++;
 				break;
 			}
@@ -78,9 +79,9 @@ void put(HashMap **ppHashMap,  char key, int val)
 			ListNode *pKeyNode = (ListNode*)calloc(1, sizeof(ListNode));	
 			ListNode *pValNode = (ListNode*)calloc(1, sizeof(ListNode));
 			pKeyNode->val = (char*)calloc(1, sizeof(char));
-			pValNode->val = (int*)calloc(1, sizeof(int));
+			pValNode->val = (signed int*)calloc(1, sizeof(signed int));
 			*(char*)(pKeyNode->val) = key;
-			*(int*)(pValNode->val) = val;
+			*(signed int*)(pValNode->val) = val;
 			pKeyCur->next = pKeyNode;
 			pValCur->next = pValNode;
 			pKeyNode = NULL;
@@ -103,7 +104,7 @@ int get(HashMap *pHashMap, char key)
 		printf("plinkedlistkey is null\n");
 		return;
 	}
-	ListNode **ppKeyIndex = pHashMap->key + hashCode;
+	ListNode **ppKeyIndex = pHashMap->key  + hashCode;
 	if(!ppKeyIndex)
 	{
 		printf("key is null\n");
@@ -112,13 +113,12 @@ int get(HashMap *pHashMap, char key)
 	
 	ListNode *pKeyMove = (*ppKeyIndex);	
 	ListNode *pValMove = *(pHashMap->val + hashCode);
-	int val = -1;
+	signed int val = -1;
 	while(pKeyMove)
 	{
-		//printf("key=%c\n", *(char*)(pKeyMove->val));
 		if(*(char*)(pKeyMove->val) == key)
 		{
-			val = *(int*)(pValMove->val);		
+			val = *(signed int*)(pValMove->val);	
 			break;
 		}
 		pKeyMove = pKeyMove->next;
@@ -131,28 +131,14 @@ void push(Queue **ppQueue, char val)
 {
 	if(!ppQueue)return;
 	ListNode *node = (ListNode*)calloc(1, sizeof(ListNode));
+	//void *memcpy ( void * dest, const void * src, size_t num );
 	node->val = (char*)calloc(1, sizeof(char));
-	*(char*)(node->val) = val;
-	printf("\npush:val=%c, node->val= %c\n",val, *(char*)(node->val));
+	memcpy(node->val, (void*)(&val), sizeof(char));
 	if(!*ppQueue || !(*ppQueue)->size)
 	{
-		if(*ppQueue && !(*ppQueue)->size)
-		{
-			printf("queue size=0, enter here\n");
-			free((*ppQueue)->top);
-			free((*ppQueue)->bottom);
-			(*ppQueue)->top = NULL;
-			(*ppQueue)->bottom = NULL;
-			realloc(*ppQueue, sizeof(Queue));
-			//free(*ppQueue);
-		}
-		else			
-			*ppQueue = (Queue*)calloc(1, sizeof(Queue));
-		printf("\npush:val=%c, node->val= %c\n",val, *(char*)(node->val));
+		if(!*ppQueue)*ppQueue = (Queue*)calloc(1, sizeof(Queue));
 		(*ppQueue)->top = node;
 		(*ppQueue)->bottom = node;
-		
-		printf("\npush: %c\n", *(char*)(((ListNode*)((*ppQueue)->top))->val));
 	}
 	else
 	{
@@ -164,36 +150,21 @@ void push(Queue **ppQueue, char val)
 
 void pop(Queue *pQueue)
 {
-	if(!pQueue)return;
-	pQueue->size--;
+	if(!pQueue || !pQueue->size)return;
 	if(pQueue->top == pQueue->bottom)
+	{//the last element
+		free(pQueue->top);
+		pQueue->top = NULL;
+		pQueue->bottom = NULL;
+	}
+	else
 	{
+		ListNode* oldNode = pQueue->top;
 		pQueue->top = ((ListNode*)(pQueue->top))->next;
-		pQueue->bottom = pQueue->top;
-		free(pQueue);
-		return;
+		free(oldNode);
+		oldNode = NULL;
 	}
-	ListNode* oldNode = pQueue->top;
-	pQueue->top = ((ListNode*)(pQueue->top))->next;
-	//printf("pop node is %d\n", oldNode->val);
-	free(oldNode);
-	oldNode = NULL;
-}
-
-
-void print(Queue *pQueue)
-{
-	printf("\n---print queue begin---\n");
-	if(!pQueue)return;
-	ListNode* pMove = pQueue->top;
-	int i=0;
-	while(pMove || i<3)
-	{
-		printf("%c,", *(char*)(pMove->val));
-		pMove = pMove->next;
-		i++;
-	}
-	printf("\n---print queue end---\n");
+	pQueue->size--;
 }
 
 int lengthOfLongestSubstring(char* s)
@@ -203,58 +174,38 @@ int lengthOfLongestSubstring(char* s)
 	HashMap *pHashMap = NULL;
 	Queue *pQueue = NULL;
 	int maxLen = 0;
-	//int count  0;
 	while(*pMove)
 	{
 		if(get(pHashMap, *pMove) == -1)
 		{
 			put(&pHashMap, *pMove, 1);
 			push(&pQueue, *pMove);
-			printf("\n---111---\n");
-			printf("pMove=%c\n", *pMove);
-			//print(pQueue);
 		}
 		else
 		{
-			printf("\n---222---\n");
-			printf("pMove=%c\n", *pMove);
-			//print(pQueue);
+			maxLen = pQueue->size > maxLen ? pQueue->size : maxLen;
 			ListNode *pTop = pQueue->top;
-			printf("111pTop->val===%c\n", *(char*)(pTop->val));
 			while(*(char*)(pTop->val) != *pMove)
 			{
+				put(&pHashMap, *(char*)(pTop->val), -1);
 				pop(pQueue);
-				//print(pQueue);
 				pTop = pQueue->top;
-				printf("222pTop->val===%c\n", *(char*)(pTop->val));
 			}
-			maxLen = pQueue->size > maxLen ? pQueue->size : maxLen;
-			printf("111queue size=%d\n",pQueue->size);
-			if(!pQueue)printf("111pQueue is null\n");
+			put(&pHashMap, *(char*)(pTop->val), -1);
 			pop(pQueue);
-			printf("222queue size=%d\n",pQueue->size);
-			if(!pQueue)printf("222pQueue is null\n");
 			push(&pQueue, *pMove);	
-			printf("333pTop->val===%c\n", *(char*)(pTop->val));
-			//print(pQueue);
+			put(&pHashMap, *pMove, 1);
 		}
 		pMove++;
 	}
-	printf("maxLen=%d\n", pQueue->size > maxLen ? pQueue->size : maxLen);
 	return pQueue->size > maxLen ? pQueue->size : maxLen;
 }
 
 
 int main(void)
 {
-	char *s = "abba";	
-/**
-	Queue *pQueue = NULL;
-	char *pMove = s;
-	while((*pMove) != '\0')
-		push(&pQueue, *pMove++);
-	print(pQueue);
-**/
-	printf("%d", lengthOfLongestSubstring(s));
+	char *s = "pwwkew";	
+	printf("%d\n", lengthOfLongestSubstring(s));
+
 	return 0;
 }
