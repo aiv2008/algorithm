fdb8a702f3532e7b39a518339c934987a7de604f
aiv2008 <aiv2005@sina.com>
2019-12-07 04:36:50 -0500

    regular expression matching testing

--- Codes/aiv2008/regular_expression_matching/regular_expression_matching.c
+++ Codes/aiv2008/regular_expression_matching/regular_expression_matching.c
@@ -13,7 +13,11 @@ bool isMatch(char *s, char *p) {
 
 bool dp(char *s, char *p, char *ps, char *pp) {
 	printf("%c, %d ,%c, %d\n", *ps, ps - s, *pp, pp - p);
-	if(*ps == '\0' && *pp == '\0') return true;
+	printf("strlen(s)=%d, ps - s=%d, strlen(p)=%d, pp - p=%d\n", strlen(s), ps - s , strlen(p), pp - p);
+	if(*ps == '\0' && *pp == '\0') {
+		printf("1111\n");
+ 		return true;
+	}
 	//判断当前字符是否匹配, 包括以下几种情况
 	//1. 当前字符相等或者p为'.', 则划分以下两种情况: 
 	//(1) 后面一个字符不为'*', 即只匹配一个, 则s和p指针都向后移动一位
@@ -27,12 +31,12 @@ bool dp(char *s, char *p, char *ps, char *pp) {
 	//1. 匹配0个,s不变, p+2
 	//2. 匹配1个, s+1, p+2
 	//3. 匹配多个, s+1 , p不变
-	if(*ps == '\0' || *pp == '\0') return false;
+	//if(*ps == '\0' || *pp == '\0') return false;
 	if(*ps == *pp || *pp == '.') {//当前字符相等或者p为'.'
 		if( *(pp + 1) != '*' ) {//后面一个字符不为'*', 即只匹配一个, 则s和p指针都向后移动一位
 			return dp(s, p, ps + 1, pp + 1);
 		}else {//后面一个字符为'*'
-			if(*ps != *(ps + 1)) {//s+1和s不相同, 则只可匹配0个或者匹配一个
+			if(*ps != *(ps + 1) || *pp != '.') {//s+1和s不相同或者p不为'.'(当p为'.'时, 可匹配任何单个字符), 则只可匹配0个或者匹配一个
 				//匹配0个
 				bool r = dp(s, p, ps, pp + 2);
 				if(!r) {
@@ -42,12 +46,15 @@ bool dp(char *s, char *p, char *ps, char *pp) {
 				return r;
 			}else {//s+1和s相同, 则可匹配0个, 1个或者多个
 				//匹配0个
+				printf("2222\n");
 				bool r = dp(s, p, ps, pp + 2);
 				if(!r) {
 					//匹配1个
+					printf("3333\n");
 					r = dp(s, p, ps + 1, pp + 2);
 					if(!r) {
 						//匹配多个
+						printf("4444\n");
 						r = dp(s, p, ps + 1, pp);
 					}
 				}
@@ -55,23 +62,23 @@ bool dp(char *s, char *p, char *ps, char *pp) {
 			}
 		}
 	} else {//当前字符不相等
-		if(*(p+1) == '*') return dp(s, p, ps, pp + 2);
+		if( *pp != '\0' && *(pp+1) == '*') return dp(s, p, ps, pp + 2);
 		else return false;
 	}
 }
 
 int main(void)
 {
-/**
+
 	char s[30], p[30];
 	printf("enter s:\n");
 	gets(s);
 	printf("enter p:\n");
 	gets(p);
-**/
 
-	char *s = "bbbba";
-	char *p = ".*a*a";
+
+	//char *s = "bbbba";
+	//char *p = ".*a*a";
 
 	printf("%d\n", isMatch(s, p));
 	return 0;
