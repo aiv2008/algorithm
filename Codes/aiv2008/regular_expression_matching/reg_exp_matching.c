ad338e0142cfa4e14ec85d7f7fdd2b4355735158
aiv2008 <aiv2005@sina.com>
2020-01-07 05:12:58 -0500

    regular expression matching using NFA, finishing the first step which is the hashmap

--- Codes/aiv2008/regular_expression_matching/reg_exp_matching.c
+++ Codes/aiv2008/regular_expression_matching/reg_exp_matching.c
@@ -5,12 +5,15 @@
 #define MAP_SIZE 7
 
 typedef struct {
+	char key;
 	int val;
+	struct Entry* this;
 	struct Entry* next;
 } Entry;
 
 typedef struct {
-	struct Entry* entry;
+	Entry entry[MAP_SIZE];
+	//Entry *entry;
 	int size;
 } HashMap;
 
@@ -19,29 +22,68 @@ int hash(char c) {
 	return ((int)c) % MAP_SIZE;
 }
 
-void push(HashMap *(*map)[MAP_SIZE], char key, int val) {
+void push(HashMap **map, char key, int val) {
+	printf("key=%c, val=%d\n", key, val);
 	if(map == NULL) { return ;}
 	if(*map == NULL) {
-		HashMap *initMap[MAP_SIZE];
-		int i;
-		for(i=0;i<MAP_SIZE;i++) {
-			initMap[i] = (int*)calloc(1, sizeof(HashMap));
-		}
-		map = initMap;
+		*map = (HashMap*)calloc(1, sizeof(HashMap));
+		(*map)->size = MAP_SIZE;
+		//(*map)->entry = (Entry*)calloc(MAP_SIZE, sizeof(Entry));
 	}
-	int hashCode = hash(c);
-	if((map[hashCode])->val == NULL) {
-		(map[hashCode])->entry = (Entry*)calloc(1, sizeof(Entry));
-		Entry *entry = (map[hashCode])->entry;
+	//printf("aaaa\n");
+	int hashCode = hash(key);
+	Entry *entry = &((*map)->entry)[hashCode];
+	if(entry->this == NULL) {
+		entry->key = key;
 		entry->val = val;
+		entry->this = entry;
 		entry->next = NULL;
 	} else {
-		Entry *entry = (Entry*)calloc(1, sizeof(Entry));
-		entry->val = val;
-		entry->next = NULL;
-		Entry *mapEntry = (map[hashCode])->entry;
-		mapEntry->next = entry;
+		Entry *pEntry = entry;
+		//Entry *pEntry = entry.this;
+		while(pEntry->next != NULL) {
+			if(pEntry->key == key) {
+				printf("11key=%c, val=%d\n", key, val);
+				pEntry->val = val;
+				break;
+			}
+			pEntry = pEntry->next;
+		}
+		//最后一个节点
+		if(pEntry->key == key) {
+	//		printf("22key=%c, val=%d\n", key, val);
+			pEntry->val = val;
+	//		printf("val1===%d\n", pEntry->val);
+		} else {
+	//		printf("33key=%c, val=%d\n", key, val);
+			Entry *newEntry = (Entry*)calloc(1, sizeof(Entry));
+			newEntry->key = key;
+			newEntry->val = val;
+			newEntry->this = newEntry;
+			newEntry->next = NULL;
+			pEntry->next = newEntry;
+		}
+		//entry.next = newEntry;
 	}
+	//printf("wait...\n");
+}
+
+int get(HashMap *map, char key) {
+	if(map == NULL) return -1;
+	int val = -1;
+	int hashCode = hash(key);
+//	printf("hashCode=%d\n", hashCode);
+	Entry entry = (map->entry)[hashCode];
+//	Entry *entry = map->entry + hashCode;
+	Entry *pEntry = &entry;
+	while(pEntry != NULL) {
+		if(pEntry->key == key) {
+			val = pEntry->val;
+			break;
+		}
+		pEntry = pEntry->next;
+	}
+	return val;
 }
 
 void test(HashMap *map[]) {
@@ -53,9 +95,18 @@ void test(HashMap *map[]) {
 }
 
 int main(void) {
-	HashMap (*a)[8];
+	HashMap *map = NULL;;
+	int b[] = {'a','b','c','d','e','f','g','h', 'a', 'c'};
 	int i;
-	
-	test(NULL);	
+	int size = sizeof(b)/sizeof(b[0]);
+	for(i=0;i<size;i++) {
+		int val = get(map, b[i]);
+//		printf("key=%c, val=%d\n", b[i], val);
+		push( &map, b[i], val == -1 ? 1 : (val + 1));
+	}
+	for(i=0;i<size;i++) {
+		printf("%d,", get(map, b[i]));
+	}
+	printf("\n");	
 	return 0;
 }
