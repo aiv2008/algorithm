bd2f65d6671ee99d785d1c14326f3f39de97ac52
aiv2008 <aiv2005@sina.com>
2020-04-10 18:21:06 +0800

    titanic statistic

---
+++ Codes/aiv2008/py_project/titanic/fill_lacked.py
@@ -0,0 +1,4 @@
+from sklearn.ensemble import RandomForestRegressor
+
+def set_missing_ages: 
+	
--- Codes/aiv2008/py_project/titanic/statistic_1.py
+++ Codes/aiv2008/py_project/titanic/statistic_1.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties #字体管理器
 
 #设置汉字格式
-font = FontProperties(fname=r"/usr/lib/python3/dist-packages/matplotlib/program_font/simhei.ttf", size=15)
+#font = FontProperties(fname=r"/usr/lib/python3/dist-packages/matplotlib/program_font/simhei.ttf", size=15)
 
-#plt.rcParams['font.sans-serif']=['simhei'] #用来正常显示中文标签
-#plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+plt.rcParams['font.sans-serif']=['simhei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 
 #fig=plt.figure()
 #fig.set(alpha=0.2)
@@ -24,35 +24,24 @@ pd.set_option('expand_frame_repr', False)
 
 data_train = pd.read_csv("data/train.csv")
 print(data_train.describe())
+print(data_train.info())
 
 plt.subplot2grid((2,4),(0,0))             # 在一张大图里分列几个小图
 data_train.Survived.value_counts().plot(kind='bar')# 柱状图 
-plt.title(u"获救情况 (1为获救)", fontproperties=font) # 标题
-plt.ylabel(u"人数", fontproperties=font) 
+plt.title(u"获救情况 (1为获救)") # 标题
+plt.ylabel(u"人数") 
 
 plt.subplot2grid((2,4),(0,1))
 data_train.Pclass.value_counts().plot(kind='bar')
-plt.title(u"等级 1最高", fontproperties=font)
-plt.ylabel(u"人数", fontproperties=font)
-
-'''
-plt.subplot2grid((2,3),(0,2))
-plt.scatter(data_train.Survived, data_train.Age)
-plt.title(u"获救情况（1为获救)", fontproperties=font)
-plt.ylabel(u"年龄分布", fontproperties=font)
-
-plt.subplot2grid((2,3),(1,0))
-plt.scatter(data_train.Pclass, data_train.Age)
-plt.title(u"等级为1 的年龄分布", fontproperties=font)
-plt.ylabel(u"人数", fontproperties=font)
-'''
+plt.title(u"等级 1最高")
+plt.ylabel(u"人数")
 
 plt.subplot2grid((2,4),(0,2), colspan=2)
 data_train.Age[data_train.Survived==1].plot(kind='kde')
 data_train.Age[data_train.Survived==0].plot(kind='kde')
-plt.title(u"获救情况年龄分布（1 获救）", fontproperties=font)
-plt.xlabel(u"年龄", fontproperties=font)
-plt.ylabel(u"密度", fontproperties=font)
+plt.title(u"获救情况年龄分布（1 获救）")
+plt.xlabel(u"年龄")
+plt.ylabel(u"密度")
 plt.legend((u"获救", u"死亡"),loc="best")
 
 
@@ -60,27 +49,35 @@ plt.subplot2grid((2,4),(1,0), colspan=2)
 data_train.Age[data_train.Pclass==1].plot(kind='kde')
 data_train.Age[data_train.Pclass==2].plot(kind='kde')
 data_train.Age[data_train.Pclass==3].plot(kind='kde')
-plt.title(u"乘客等级分布情况", fontproperties=font)
-plt.xlabel(u"年龄", fontproperties=font)
-plt.ylabel(u"密度", fontproperties=font)
+plt.title(u"乘客等级分布情况")
+plt.xlabel(u"年龄")
+plt.ylabel(u"密度")
 plt.legend((u"头等", u"二等", u"三等"),loc="best")
 
 
 plt.subplot2grid((2,4),(1,2))
 data_train.Embarked.value_counts().plot(kind="bar")
-plt.title(u"登陆码头分布情况", fontproperties=font)
-plt.ylabel(u"人数", fontproperties=font)
+plt.title(u"登陆码头分布情况")
+plt.ylabel(u"人数")
 
+'''
 plt.subplot2grid((2,4),(1,3))
 survived_0 = data_train.Pclass[data_train.Survived==0].value_counts()
 survived_1 = data_train.Pclass[data_train.Survived==1].value_counts()
 df=pd.DataFrame({u"死亡": survived_0, u"存活": survived_1})
 df.plot(kind="bar", stacked=True)
-plt.title(u"乘客等级获救分布情况", fontproperties=font)
-plt.xlabel(u"等级",  fontproperties=font)
-plt.ylabel(u"获救情况（1 获救）",  fontproperties=font)
+plt.title(u"乘客等级获救分布情况")
+plt.xlabel(u"等级" )
+plt.ylabel(u"获救情况（1 获救）" )
+'''
 
-
-
+plt.subplot2grid((2,4),(1,3))
+sex_survived_0 = data_train.Sex[data_train.Survived==0].value_counts()
+sex_survived_1 = data_train.Sex[data_train.Survived==1].value_counts()
+sex_df = pd.DataFrame({u"死亡":sex_survived_0, u"获救":sex_survived_1})
+sex_df.plot(kind="bar", stacked=True)
+plt.title(u"性别获救情况分布")
+plt.xlabel(u"性别")
+plt.ylabel(u"密度")
 
 plt.show() 
