4f219bc29410a9dd1ca866fd70ecffbfab68b6dc
anson <aiv2005@sina.com>
2020-07-10 18:00:45 +0800

    leetcode63 finished, but outputs is error

--- Codes/aiv2008/leetcode63-unique-paths-with-obstacles/unique-paths-with-obstacles.c
+++ Codes/aiv2008/leetcode63-unique-paths-with-obstacles/unique-paths-with-obstacles.c
@@ -1,29 +1,70 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int uniquePathsWithObstacles(int m, int n){
+int uniquePaths(int m, int n){
 	int i;
 	int j;
-	if(m==1) return 1;
 	int dp[n];
 	for(i=0;i<n;i++) dp[i] = 1;
 	for(i=1;i<m;i++){
 		for(j=1;j<n;j++){
-			if(j==)
 			dp[j] = dp[j] + dp[j-1];
 		}
 	}
 	return dp[n-1];			
 }
 
+int uniquePathsWithObstacles(int** obstacleGrid, int obstacleGridSize, int* obstacleGridColSize){
+	if(obstacleGridSize == 0 || *obstacleGridColSize == 0) return 0;
+	int i;
+	int j;
+	int dp[*obstacleGridColSize];
+	for(i=0;i<*obstacleGridColSize;i++) {
+		if(*(*obstacleGrid+i) == 1) dp[i] = 0;
+		else dp[i] = 1;
+	}
+
+	for(i=0;i<*obstacleGridColSize;i++) {
+		printf("dp=%d,", dp[i]);
+	}
+	printf("\n");
+	
+
+	for(i=1;i<obstacleGridSize;i++) {
+		if(*(*(obstacleGrid+i)) == 1) dp[0] = 0;
+		for(j=0;j<*obstacleGridColSize;j++){
+			if(*(*(obstacleGrid+i)+j) == 1) dp[j] = 0;
+			else {
+				if(j==0){
+					printf("dp=%d,", dp[j]);
+					continue;
+				}
+				else dp[j] = dp[j] + dp[j-1];
+			}
+			printf("dp=%d,", dp[j]);
+		}
+		printf("\n");
+	}
+	return dp[*obstacleGridColSize-1];
+	
+}
+
 int main(void){
-	int m,n;
-	printf("input m:\n");
-	scanf("%d", &m);
-	printf("input n:\n");
-	scanf("%d", &n);
-	//int m = 50, n = 50;
-	printf("result is %d\n", uniquePaths(m,n));
-	printf("result is %d\n", uniquePathsEx(m,n));
-	printf("result is %d\n", uniquePathsEx1(m,n));
+	int m = 3;
+	int n = 3;
+	int a[][3] = {{0,0,0},{0,1,0},{0,0,0}};
+	int** b = (int**)calloc(m, sizeof(int*));
+	//int rowSize = sizeof(a)/sizeof(a[0]);
+	printf("m=%d\n", m);
+	int colSize[m];
+	int i;
+	for(i=0;i<m;i++){
+		colSize[i] = sizeof(a[i]) / sizeof(a[i][0]);	
+		*(b+i) = (int*)calloc(colSize[i], sizeof(int));
+		int j;
+		for(j =0;j<n;j++) *(*(b+i)+j) = a[i][j];
+	}
+	int result = uniquePathsWithObstacles(b, m, colSize);
+	printf("result is %d\n", result);
+	return 0;
 }
