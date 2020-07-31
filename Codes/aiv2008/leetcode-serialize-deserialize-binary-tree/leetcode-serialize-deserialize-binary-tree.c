5c480e0307e43f984ffd798316f59198f41237f2
anson <aiv2005@sina.com>
2020-07-31 17:49:00 +0800

    leetcode serialize binary tree

--- Codes/aiv2008/leetcode-serialize-deserialize-binary-tree/leetcode-serialize-deserialize-binary-tree.c
+++ Codes/aiv2008/leetcode-serialize-deserialize-binary-tree/leetcode-serialize-deserialize-binary-tree.c
@@ -18,7 +18,7 @@ void cVectorAdd(CVector** vector, char element){
                 (*vector)->capacity = 8;
                (*vector)->size = 0;
         }
-        if((*vector)->size == (*vector)->capacity) {
+        if((*vector)->size == (*vector)->capacity-1) {//最后一个位置存放'\0'
                 int capacity = (*vector)->capacity;
                 (*vector)->capacity = capacity + capacity << 1;
                 char* newElement = (char*)calloc((*vector)->capacity, sizeof(char));
@@ -28,15 +28,42 @@ void cVectorAdd(CVector** vector, char element){
         }
         *((*vector)->element + (*vector)->size) = element;
         (*vector)->size++;
+        *((*vector)->element + (*vector)->size) = '\0';
 }
 
 int cVectorSize(CVector* vector){
 	return vector == NULL ? 0 : vector->size;
 }
 
+char cVectorGet(CVector* vector, int index){
+	return vector == NULL ? '\0' : *(vector->element+index);
+}
+
 char* cVectorAll(CVector* vector) {
 	return vector == NULL ? NULL : vector->element;
 }
+
+void cVectorIterator(CVector* vector){
+	char* c = cVectorAll(vector);
+	char* p = c;
+	while(*p != '\0'){
+		printf("%c", *p++);
+	}
+	printf("\n");
+}
+
+//整形转换成字符型
+/**
+char* itoa(int i){
+	int a = i < 0 ? -i : i;
+	if(a/10) {
+		while(a){
+			
+		}	
+	}
+	
+}
+**/
 
 char* serialize(struct TreeNode* root) {
 	CVector *cVct = NULL;
@@ -55,11 +82,14 @@ char* serialize(struct TreeNode* root) {
 				cVectorAdd(&cVct , 'l');	
 			} else {
 				int r = node->val;
-				while(r) {
-					cVectorAdd(&cVct, r/10);
-					r = r - r/10;
+				if(r/10){
+					while(r) {
+						cVectorAdd(&cVct, (char)(r/10+48));
+						r = r - r/10;
+					}
 				}
-				cVectorAdd(&cVct, (char)r);
+				cVectorAdd(&cVct, (char)(r+48));
+				cVectorIterator(cVct);
 			}
 			if(node->left != NULL && node->right != NULL) {
 				vectorAdd(&vector, node->left);
@@ -69,7 +99,8 @@ char* serialize(struct TreeNode* root) {
 		}
 	}
 
-	cVectorAdd(&cVct , ']');	
+	cVectorAdd(&cVct , ']');
+	cVectorAdd(&cVct , '\0');	
 	return cVct == NULL ? NULL : cVct->element;
 }
 
@@ -81,6 +112,8 @@ struct TreeNode* deserialize(char* data) {
 int main(void){
 //	int a[] = {2,34,5,6,7,3,2,4,56,7,8,39,4};
 //	int a[] = {2,3,54,56,6,67,8};
+
+
 	int a[] = {1,2,3,-1,-1,4,5};
 	Vector* vector = NULL;
 	int i;
@@ -104,8 +137,37 @@ int main(void){
 		jNode->right = iNode;
 		j++;
 	}
+	struct TreeNode* root = (struct TreeNode*)vectorGet(vector, 0);
+	treeIterator(root);
+	char* c = serialize(root);
+	char* p = c;
+	while(*p != '\0'){
+		printf("%c", *p);
+		p++;	
+	}
+	printf("\n");
 
-	treeIterator((struct TreeNode*)vectorGet(vector, 0));
+
+/**
+	int c[] = {1,2,3,4,5,6,7};
+	int size = sizeof(c) / sizeof(c[0]);
+	int i;
+	CVector *vector = NULL;
+	for(i=0;i<size;i++){
+		printf("%c,", (char)c[i]);
+		cVectorAdd(&vector, (char)c[i]);
+		cVectorAdd(&vector, ',');
+	}
+	printf("\n");
+	cVectorAdd(&vector, '\0');
+		
+	char* cc = cVectorAll(vector);
+	cVectorIterator(vector);	
+**/
+
+
+//	int a = 1;
+//	printf("%c\n", (char)a);
 
 	return 0;
 }
