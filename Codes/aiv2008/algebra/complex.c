9ff27928227511351228d65514f6dbfc9afa4c92
anson <aiv2005@sina.com>
2020-08-21 17:25:48 +0800

    complex

---
+++ Codes/aiv2008/algebra/complex.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef struct {
+	double real;
+	double virtual;
+}Complex;
+
+
+Complex* complexPlus(Complex* a, Complex* b){
+
+}
+
+Complex* complexMinus(Complex* a, Complex* b){
+
+}
+
+Complex* complexMultiple(Complex* a, Complex* b){
+
+
+}
+
+Complex* complexDivided(Complex* a, Complex* b){
+
+}
